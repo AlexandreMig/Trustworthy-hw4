879ddd0eb5e761eb64a9f7c6b95d54cb3c76e38e
Alexandre <75760260+AlexandreMig@users.noreply.github.com>
2023-04-23 16:54:38 -0400

    commit

--- bob.c
+++ bob.c
@@ -252,34 +252,30 @@ void Convert_to_Hex(char output[], unsigned char input[], int inputlength)
 /*============================
         Sending via ZeroMQ 
 ==============================*/
-void Send_via_ZMQ(unsigned char send[], int sendlen)
-{
-	void *context = zmq_ctx_new ();					            //creates a socket to talk to Bob
-    void *requester = zmq_socket (context, ZMQ_REQ);		    //creates requester that sends the messages
-   	printf("Connecting to Bob and sending the message...\n");
-    zmq_connect (requester, "tcp://localhost:5555");		    //make outgoing connection from socket
-    zmq_send (requester, send, sendlen, 0);			    	    //send msg to Bob
-    zmq_close (requester);						                //closes the requester socket
-    zmq_ctx_destroy (context);					                //destroys the context & terminates all 0MQ processes
+void Send_via_ZMQ(unsigned char send1[], int sendlen1, unsigned char send2[], int sendlen2) {
+    void *context = zmq_ctx_new();
+    void *requester = zmq_socket(context, ZMQ_REQ);
+    printf("Connecting to Bob and sending the messages...\n");
+    zmq_connect(requester, "tcp://localhost:5555");
+    zmq_send(requester, send1, sendlen1, ZMQ_SNDMORE);
+    zmq_send(requester, send2, sendlen2, 0);
+    zmq_close(requester);
+    zmq_ctx_destroy(context);
 }
 
 /*============================
         Receiving via ZeroMQ 
 ==============================*/
-unsigned char *Receive_via_ZMQ(unsigned char receive[], int *receivelen, int limit) 
-{
-	void *context = zmq_ctx_new ();			        	                                 //creates a socket to talk to Alice
-    void *responder = zmq_socket (context, ZMQ_REP);                                   	//creates responder that receives the messages
-   	int rc = zmq_bind (responder, "tcp://*:5555");	                                	//make outgoing connection from socket
-    int received_length = zmq_recv (responder, receive, limit, 0);	                  	//receive message from Alice
-    unsigned char *temp = (unsigned char*) malloc(received_length);
-    for(int i=0; i<received_length; i++){
-        temp[i] = receive[i];
-    }
-    *receivelen = received_length;
-    printf("Received Message: %s\n", receive);
-    printf("Size is %d\n", received_length);
-    return temp;
+void Receive_via_ZMQ(unsigned char *receive1, int *receivelen1, unsigned char *receive2, int *receivelen2, int limit) {
+    void *context = zmq_ctx_new();
+    void *responder = zmq_socket(context, ZMQ_REP);
+    int rc = zmq_bind(responder, "tcp://*:5555");
+    int received_length1 = zmq_recv(responder, receive1, limit, 0);
+    *receivelen1 = received_length1;
+    int received_length2 = zmq_recv(responder, receive2, limit, 0);
+    *receivelen2 = received_length2;
+    zmq_close(responder);
+    zmq_ctx_destroy(context);
 }
 
 void Save_Signature(char filename[], unsigned char sig[], int sig_len) {
@@ -340,8 +336,7 @@ int main (int argc, char* argv[])
     EC_POINT_hex2point(EC_KEY_get0_group(alice_dsa_pk), alice_dsa_pk_hex, alice_dsa_pk_point, NULL);
     EC_KEY_set_public_key(alice_dsa_pk, alice_dsa_pk_point);
 
-    
-     // 3. Sign Bob's ECDH public key
+    // 3. Sign Bob's ECDH public key
     unsigned char sig_b[ECDSA_size(bob_dsa)];
     unsigned int sig_b_len;
     int ecdh_pub_b_oct_len = EC_POINT_point2oct(EC_KEY_get0_group(bob_dh), EC_KEY_get0_public_key(bob_dh), EC_KEY_get_conv_form(bob_dh), NULL, 0, NULL);
@@ -357,24 +352,15 @@ int main (int argc, char* argv[])
     // Save Bob's signature to a file
     Save_Signature("Signature_Bob.txt", sig_b, sig_b_len);
 
+    // 4. Send ECDH public key and signature to Alice
+    Send_via_ZMQ((unsigned char *)ecdh_pub_b_hex, strlen((const char *)ecdh_pub_b_hex), sig_b, sig_b_len);
 
-    // Prepare ZeroMQ context and socket
-    void *context = zmq_ctx_new();
-    void *socket = zmq_socket(context, ZMQ_PAIR);
-    zmq_connect(socket, "tcp://localhost:5555");
-
-    // Send ECDH public key and signature to Alice
-    zmq_send(socket, ecdh_pub_b_hex, strlen((const char *)ecdh_pub_b_hex), 0);
-    zmq_send(socket, sig_b, sig_b_len, 0);
-
-    // 4. Receive Alice's ECDH public key and signature
+    // 5. Receive Alice's ECDH public key and signature
     char ecdh_pub_a_hex[131];
     unsigned char sig_a[ECDSA_size(alice_dsa_pk)];
     int sig_a_len;
-
-    zmq_recv(socket, ecdh_pub_a_hex, 131, 0);
+    Receive_via_ZMQ((unsigned char *)ecdh_pub_a_hex, &sig_a_len, sig_a, &sig_a_len, 131);
     ecdh_pub_a_hex[130] = '\0';
-    sig_a_len = zmq_recv(socket, sig_a, ECDSA_size(alice_dsa_pk), 0);
 
     // 5. Verify Alice's signature on her ECDH public key
     unsigned char hash_a[SHA256_DIGEST_LENGTH];
